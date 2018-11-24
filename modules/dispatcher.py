44b319013a3526b1c31e1addd8363e7cd9e06270
Gustavo Henrique Fernandes Carvalho <gustavohfcarvalho@gmail.com>
2018-11-24 18:22:13 -0200

    Adicionando gerenciador de IO

--- modules/dispatcher.py
+++ modules/dispatcher.py
@@ -2,6 +2,7 @@ from modules import file_manager as fm
 from modules import process_manager as pm
 from modules import memory_manager as mm
 from modules import queue_manager as qm
+from modules import io_manager as im
 
 
 class Dispatcher:
@@ -11,6 +12,7 @@ class Dispatcher:
         self.fileManager = fm.FileManager()
         self.memoryManager = mm.MemoryManager()
         self.queueManager = qm.QueueManager()
+        self.ioManager = im.IoManager()
 
         self.runningProcess = None
 
@@ -29,12 +31,19 @@ class Dispatcher:
                 if process.tempo_de_inicializacao > time:
                     break
 
-                if self.memoryManager.alloc_memory(process):
+                io_get_result = self.ioManager.getIOdevice(process)
+                if not io_get_result:
+                    continue
+
+                memory_alloc_result = self.memoryManager.alloc_memory(process)
+                if memory_alloc_result:
                     self.processManager.processes_to_start.remove(process)
                     process.PID = PID
                     PID += 1
                     self.queueManager.add_new_process(process)
                     process.print()
+                else:
+                    self.ioManager.releaseIOdevice(process)
 
             self.runningProcess = self.queueManager.get_next_running_process(self.runningProcess)
 
@@ -48,6 +57,7 @@ class Dispatcher:
                 if self.runningProcess.cpu_time_ended():
                     self.memoryManager.free_memory(self.runningProcess)
                     self.queueManager.remove_process(self.runningProcess)
+                    self.ioManager.releaseIOdevice(process)
                     self.runningProcess = None
 
             time += 1
---
+++ modules/io_manager.py
@@ -0,0 +1,57 @@
+
+class IoManager:
+    def __init__(self):
+        self.printer_1_used_by = -1
+        self.printer_2_used_by = -1
+        self.disk_1_used_by = -1
+        self.disk_2_used_by = -1
+        self.scanner_used_by = -1
+        self.modem_used_by = -1
+
+    def getIOdevice(self, process):
+        if (process.numero_codigo_da_impresora_requisitada == 1 and self.printer_1_used_by != -1) or\
+            (process.numero_codigo_da_impresora_requisitada == 2 and self.printer_2_used_by != -1) or\
+            (process.numero_codigo_do_disco == 1 and self.disk_1_used_by != -1) or\
+            (process.numero_codigo_do_disco == 2 and self.disk_2_used_by != -1) or\
+            (process.requisicao_do_scanner == 1 and self.scanner_used_by != -1) or\
+            (process.requisicao_do_modem == 1 and self.modem_used_by != -1):
+            return False
+
+        if process.numero_codigo_da_impresora_requisitada == 1:
+            self.printer_1_used_by = process.PID
+
+        if process.numero_codigo_da_impresora_requisitada == 2:
+            self.printer_2_used_by = process.PID
+
+        if process.numero_codigo_do_disco == 1:
+            self.disk_1_used_by = process.PID
+
+        if process.numero_codigo_do_disco == 2:
+            self.disk_2_used_by = process.PID
+
+        if process.requisicao_do_scanner == 1:
+            self.scanner_used_by = process.PID
+
+        if process.requisicao_do_modem == 1:
+            self.modem_used_by = process.PID
+
+        return True
+
+    def releaseIOdevice(self, process):
+        if self.printer_1_used_by == process.PID:
+            self.printer_1_used_by = -1
+
+        if self.printer_2_used_by == process.PID:
+            self.printer_2_used_by = -1
+
+        if self.disk_1_used_by == process.PID:
+            self.disk_1_used_by = -1
+
+        if self.disk_2_used_by == process.PID:
+            self.disk_2_used_by = -1
+
+        if self.scanner_used_by == process.PID:
+            self.scanner_used_by = -1
+
+        if self.modem_used_by == process.PID:
+            self.modem_used_by = -1
